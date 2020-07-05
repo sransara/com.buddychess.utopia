537ae61972eb0c7906cc94100c91ac84c76b4644
sransara <git@sransara.com>
2020-07-05 05:25:37 -0700

    prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  svelteSortOrder: "scripts-styles-markup",
+  svelteStrictMode: true,
+  svelteAllowShorthand: false,
+  svelteBracketNewLine: false,
+};
