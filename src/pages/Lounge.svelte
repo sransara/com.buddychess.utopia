6696ea481e80895442aa0cd14398016920dcda86
sransara <git@sransara.com>
2020-07-20 22:08:56 -0700

    lounge basic UI

--- src/pages/Lounge.svelte
+++ src/pages/Lounge.svelte
@@ -1,11 +1,85 @@
 <script lang="typescript">
-  import { link } from "svelte-spa-router";
+  import { fade } from "svelte/transition";
+  import { cubicOut } from "svelte/easing";
+
+  let isCreatingPublicRoom = false;
+  let isJoiningPublicRoom = false;
+  let isCreatingPrivateRoom = false;
+  let isJoiningPrivateRoom = false;
 </script>
 
 <style>
-  a {
-    color: blue;
+  .active {
+    @apply text-2xl;
   }
 </style>
 
-<a href="/game" use:link>Go to game page</a>
+<div class="p-1">
+  <ul class="flex mb-3 text-lg">
+    <li class="mr-3">
+      <button
+        class="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 focus:outline-none"
+        class:active="{isCreatingPublicRoom}"
+        on:click="{() => {
+          isCreatingPublicRoom = true;
+          isJoiningPublicRoom = false;
+          isCreatingPrivateRoom = false;
+          isJoiningPrivateRoom = false;
+        }}"
+      >
+        Create new public room
+      </button>
+    </li>
+    <li class="mr-3">
+      <button
+        class="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 focus:outline-none"
+        class:active="{isJoiningPublicRoom}"
+        on:click="{() => {
+          isCreatingPublicRoom = false;
+          isJoiningPublicRoom = true;
+          isCreatingPrivateRoom = false;
+          isJoiningPrivateRoom = false;
+        }}"
+      >
+        Join a public room
+      </button>
+    </li>
+    <li class="mr-3">
+      <button
+        class="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 focus:outline-none"
+        class:active="{isCreatingPrivateRoom}"
+        on:click="{() => {
+          isCreatingPublicRoom = false;
+          isJoiningPublicRoom = false;
+          isCreatingPrivateRoom = true;
+          isJoiningPrivateRoom = false;
+        }}"
+      >
+        Create new private room
+      </button>
+    </li>
+    <li class="mr-3">
+      <button
+        class="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 focus:outline-none"
+        class:active="{isJoiningPrivateRoom}"
+        on:click="{() => {
+          isCreatingPublicRoom = false;
+          isJoiningPublicRoom = false;
+          isCreatingPrivateRoom = false;
+          isJoiningPrivateRoom = true;
+        }}"
+      >
+        Join a private room
+      </button>
+    </li>
+  </ul>
+  {#if isCreatingPublicRoom}
+    <div class="w-full" in:fade></div>
+  {:else if isJoiningPublicRoom}
+    <div class="w-full" in:fade></div>
+  {:else if isCreatingPrivateRoom}
+    <div class="w-full" in:fade></div>
+  {:else if isJoiningPrivateRoom}
+    <div class="w-full" in:fade></div>
+  {/if}
+</div>
